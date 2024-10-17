4cc694ba7f5d9890f161319aabe355c94d894a91
AMK1215 <aungmyokhaing1215@gmail.com>
2024-10-17 09:22:25 +0630

    testing

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,10 @@ use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('pupc', [RegisterController::class, 'pgetb']);
 Route::post('playbalance', [RegisterController::class, 'Pbalance'])->name('PCredit');
+Route::get('test', function () {
+        dd('here');
+    })->name('test');
+
 require_once __DIR__.'/admin.php';
 
 Auth::routes();
